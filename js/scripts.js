40bfa1bd985a2f971273958864291299423c837a
Alex Larson and Polina Nenchev <polina.mihova@yahoo.com>
2016-01-28 13:23:16 -0800

    specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,98 @@
-
-
-var ChoseWord = function () {
-  var array = ["pizza", "cat", "house", "poppy", "car", "orange"];
-  var pickNumber = Math.floor(Math.random()*(6-0));
-  var pickWord = array[pickNumber];
-  return pickWord;
-}
-
-function Word(example, letter, guesses) {
+function Word(example) {
     this.example = example;
-    this.letter = letter;
-    this.guesses = guesses;
-    this.currentWord = [];
+    this.letter = "";
+    this.wrongGuesses = 0;
+    this.correctGuesses = this.example.length;
+    this.guessedLetters = [];
 };
 
-Word.prototype.chop = function(word) {
-    var firstWord = word.split("");
+
+Word.prototype.chop = function() {
+    var firstWord = this.example.split("");
     return firstWord;
 };
 
-Word.prototype.replaceWord = function(word) {
-    var newWord = new Word(word);
-    var chopWord = newWord.chop(word);
-    var wordExample = [];
+Word.prototype.wrongGuesses = function() {
+  this.wrongGuesses = this.wrongGuesses +1 ;
+    return this.wrongGuesses;
+
+};
+Word.prototype.correctGuesses = function() {
+  this.correctGuesses = this.correctGuesses  -1 ;
+        return this.correctGuesses;
+};
+
+var loopLetters = function(letter, word) {
+  var gotletter = false;
+  var array = [];
+  word.guessedLetters.push(letter);
+  for(var i = 0; i<=word.example.length; i = i+1 ){
+    if(word.example[i] === letter) {
+      array.push(i)
+      foundLetter = true;
+      }
+    }
+    if (gotletter === false) {
+      return gotletter;
+    }else {
+    return array;
+};
+}
+
+//
+Word.prototype.replaceWord = function() {
+
+    var chopWord = this.example;
     for (var i = 1; i <= chopWord.length; i++) {
-        wordExample.push("_");
+        this.guessedLetters.push("_");
     };
-    var joinword  = wordExample.join(" ");
+    var joinword  = this.guessedLetters.join(" ");
     return joinword;
 };
 
-Word.prototype.count = function(word) {
-    var newWord = new Word(word);
-    var chopWord = newWord.chop(word);
-    var wordLength = chopWord.length;
-    return wordLength;
+Word.prototype.count = function() {
+  // debugger;
+  console.log(this.example)
+return this.example.length
 };
 
-Word.prototype.result = function(word, letter, guesses) {
-    var newWord = new Word(word, letter, guesses);
-    debugger;
-    if (this.guesses >= newWord.count(word)) {
-        return "you lose";
-    } else {
-        return "keep guessing";
-    }
-}
+// Word.prototype.result = function(word, letter, guesses) {
+//     var newWord = new Word(word, letter, guesses);
+//     debugger;
+//     if (this.guesses >= newWord.count(word)) {
+//         return "you lose";
+//     } else {
+//         return "keep guessing";
+//     }
+// }
+//
+// Word.prototype.guess = function(word, letter, guesses) {
+//   var newGuess = new Word(word, letter, guesses);
+//   var wordLength = newGuess.chop(word)
+//   console.log(wordLength.length)
+//     for(var i = 0; i<=wordLength.length; i = i+1 ){
+//         if(this.letter === wordLength[i]) {
+//           console.log(wordLength[i])
+//           this.currentWord.push(letter)
+//         }
+//     }
+//     return this.currentWord;
+// };
 
-Word.prototype.guess = function(word, letter, guesses) {
-  var newGuess = new Word(word, letter, guesses);
-  var wordLength = newGuess.chop(word)
-  console.log(wordLength.length)
-    for(var i = 0; i<=wordLength.length; i = i+1 ){
-        if(this.letter === wordLength[i]) {
-          console.log(wordLength[i])
-          this.currentWord.push(letter)
-        }
-    }
-    return this.currentWord;
-};
 
-
-$(function() {
-
-var word = ChoseWord();
-var newWord = new Word(word);
-this.currentWord = newWord.replaceWord(word);
-$("#word-display").text(this.currentWord);
-$("#hiddenWord").append(word)
-
-$("form").submit(function(event) {
-var inputLetter = $("#letters").val();
-var results = newWord.guess(word,inputLetter,guesses);
-
-$("#guesses").text(results);
-        event.preventDefault();
-    })
-
-})
+// $(function() {
+//
+//   // var word = ChoseWord();
+//   // var newWord = new Word(word);
+//   // this.currentWord = newWord.replaceWord(word);
+//   $("#word-display").text(this.currentWord);
+//   $("#hiddenWord").append(word)
+//
+//   $("form").submit(function(event) {
+//     var inputLetter = $("#letters").val();
+//     var results = newWord.guess(word,inputLetter,guesses);
+//   });
+// });
+// $("#guesses").text(results);
+//         event.preventDefault();
+//     });
--- spec/specs.js
+++ spec/specs.js
@@ -1,26 +1,60 @@
 describe("Word", function() {
-    it("will display its word", function() {
+    it("will create a word object with given properties", function() {
         var newWord = new Word("dog");
         expect(newWord.example).to.equal("dog");
+        expect(newWord.wrongGuesses).to.equal(0);
+        expect(newWord.guessedLetters).to.eql([]);
     });
+
     it("will chop up a word into its letters", function() {
         var newWord = new Word("dog");
         expect(newWord.chop("dog")).to.eql(["d", "o", "g"]);
     });
+
     it("will replace letters with underscore", function() {
         var newWord = new Word("dog");
         expect(newWord.replaceWord("dog")).to.eql("_ _ _");
     });
+
     it("will count total number of letters in the word", function() {
         var newWord = new Word("dog");
         expect(newWord.count("dog")).to.equal(3);
     });
-    it("will tell you if you lose", function() {
-        var newWord = new Word("dog", "d", 9);
-        expect(newWord.result("dog", "z", 9)).to.equal("you lose");
+
+    it("will check how many guesses are wrong", function() {
+      var newWord = new Word("dog");
+      expect(newWord.wrongGuesses).to.equal(0);
     });
-    it("will tell you if you should keep guessing", function() {
-        var newWord = new Word("dog", "d", 0);
-        expect(newWord.result("dog", "z", 0)).to.equal("keep guessing");
-    });
+
+    // it("will tell you if you lose", function() {
+    //     var newWord = new Word("dog", "d", 9);
+    //     expect(newWord.result("dog", "z", 9)).to.equal("you lose");
+    // });
+    //
+    // it("will tell you if you should keep guessing", function() {
+    //     var newWord = new Word("dog", "d", 0);
+    //     expect(newWord.result("dog", "z", 0)).to.equal("keep guessing");
+    // });
+});
+
+
+
+describe('loopLetter', function() {
+  it('will search the loopcreated and if the guess is wrong will give the user false', function(){
+    var newWord = new Word("house");
+    expect(loopLetter('t', newWord)).to.equal(false);
+  });
+
+  it('will search the loopcreated for correct guess and will return an array with the locations of that letter', function() {
+    var newWord = new Word("house");
+    expect(loopLetter('h', newWord)).to.eql([0,5]);
+  });
+
+//   it('will add guessed letters to the usedLetters array', function() {
+//     var testGame = new Game("falafel");
+//     findLetter('r', testGame);
+//     findLetter('f', testGame);
+//     findLetter('s', testGame);
+//     expect(testGame.usedLetters).to.eql(['r', 'f', 's']);
+//   });
 });
